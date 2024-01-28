5491073d84b1ab82c2a6c1c93e870f2ad7b5f9fe
YoutonneDev <157954312+YoutonneDev@users.noreply.github.com>
2024-01-28 10:58:16 +0100

    ajout des fonctions

---
+++ Fonctions/generateCaptcha.js
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////       CREATE BY          ////////////////////////////////////////
+///////////////////////////////       YOUTONNE          ////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+const Discord = require("discord.js");
+const Canvas = require("canvas");
+
+module.exports = async () => {
+
+    let caracters = [..."1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    let text = [];
+    for(let i = 0; i < 6; i++) text.push(caracters[Math.floor(Math.random() * caracters.length)]);
+    text = text.join("");
+
+    const canvas = Canvas.createCanvas(300, 150)
+    const ctx = canvas.getContext("2d");
+
+    ctx.font = '35px "Comic Sans MS"';
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(text, (150 - (ctx.measureText(text).width) / 2), 85)
+
+    return {canvas: canvas, text: text}
+}
